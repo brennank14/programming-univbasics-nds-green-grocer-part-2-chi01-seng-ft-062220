4cfffd8285adf2aa525b00717a12e0458c89164e
Kathleen Brennan <brennank14@gmail.com>
2020-06-17 23:53:42 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,14 +6,15 @@ def apply_coupons(cart, coupons)
     coupons.each  do |c_element|
       if c_element[:item] == element[:item]
         if element[:count] >= c_element[:num]
-        element[:count] -= c_element[:num]
-      else
-        element[:count] == 0
+          element[:count] -= c_element[:num]
+        else
+          element[:count] == 0
+        end
+        cart << {:item => "#{c_element[:item]} W/COUPON", :count => c_element[:num], :price => (c_element[:cost])/(c_element[:num]), :clearance => element[:clearance]}
       end
-      cart << {:item => "#{c_element[:item]} W/COUPON", :count => c_element[:num], :price => (c_element[:cost])/(c_element[:num]), :clearance => element[:clearance]}
     end
-    cart
   end
+  cart 
 end
 
 def apply_clearance(cart)
